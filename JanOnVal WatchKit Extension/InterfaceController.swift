4be6c13f6795078e7e9de0cbe9ff53988f9f251a
Christian Stolz <christian.stolz@janitza.de>
2017-12-08 07:36:20 +0100

    Initial Commit

---
+++ JanOnVal WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  JanOnVal WatchKit Extension
+//
+//  Created by Christian Stolz on 08.12.17.
+//  Copyright © 2017 Andreas Mueller. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
